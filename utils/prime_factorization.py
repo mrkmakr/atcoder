fdf75c627924ca1c43d84bc4076d793a32c9e18e
mrkmakr <mrkmakr123@gmail.com>
2020-04-05 02:25:53 +0900

    abc161ef

--- utils/prime_factorization.py
+++ utils/prime_factorization.py
@@ -16,3 +16,16 @@ def factorize(n):
     if tmp != 1:
         result.append([tmp, 1])
     return result
+
+def divisorize(n):
+    ## 約数
+    # arr = factorize(20)
+    # arr == [2, 4, 5, 10, 20]
+    f = factorize(n)
+    divisors = [1]
+    for k in range(len(f)):
+        num_divisors = len(divisors)
+        for i in range(1, f[k][1] + 1):
+            for j in range(num_divisors):
+                divisors.append(divisors[j] * f[k][0] ** i)
+    return sorted(divisors)
